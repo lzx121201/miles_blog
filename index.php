5fef9fb59288386735c5b5604803c4c199e27210
lizhengxing <lizhengxing@localhost>
2017-04-16 15:14:16 +0100

    test

---
+++ index.php
@@ -0,0 +1,213 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+
+        <title>Agency - Start Bootstrap Theme</title>
+
+        <!-- Bootstrap Core CSS -->
+        <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+        <!-- Custom Fonts -->
+        <link href="vendor/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css">
+        <link href='https://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+        <link href='https://fonts.googleapis.com/css?family=Droid+Serif:400,700,400italic,700italic' rel='stylesheet' type='text/css'>
+        <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700' rel='stylesheet' type='text/css'>
+        <link href="css/index.css" rel="stylesheet" type="text/css"/>
+
+        <!-- Theme CSS -->
+        <link href="css/agency.min.css" rel="stylesheet">
+
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js" integrity="sha384-0s5Pv64cNZJieYFkXYOTId2HMA2Lfb6q2nAcx2n0RTLUnCAoTTsS0nKEO27XyKcY" crossorigin="anonymous"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js" integrity="sha384-ZoaMbDF+4LeFxg6WdScQ9nnR1QC2MIRxA1O9KWEXQwns1G8UNyIEZIQidzb0T1fo" crossorigin="anonymous"></script>
+        <![endif]-->
+
+    </head>
+
+    <body id="page-top" class="index">
+        <ul class="cb-slideshow">
+            <li><span class="a">Image 01</span></li>
+            <li><span class="b">Image 02</span></li>
+            <li><span class="a">Image 03</span></li>
+            <li><span class="b">Image 04</span></li>
+            <li><span class="a">Image 05</span></li>
+            <li><span class="b">Image 06</span></li>
+        </ul>
+        <!-- Navigation -->
+        <nav id="mainNav" class="navbar navbar-default navbar-custom navbar-fixed-top">
+            <div class="container">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header page-scroll">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
+                    </button>
+                    <a class="navbar-brand page-scroll" href="#page-top"></a>
+                </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li class="hidden">
+                            <a href="#page-top"></a>
+                        </li>
+                        <li>
+                            <a class="page-scroll" href="#service">Login</a>
+                        </li>
+                        <li>
+                            <a class="page-scroll" href="#portfolio">Register</a>
+                        </li>
+                        <li>
+                            <a class="page-scroll" href="#team">Team</a>
+                        </li>
+                    </ul>
+                </div>
+                <!-- /.navbar-collapse -->
+            </div>
+            <!-- /.container-fluid -->
+        </nav>
+
+        <!-- Header -->
+        <header id="top" class="header">
+            <div class="text-vertical-center">
+                <center><h1 class="quote">Miles</h1></center>
+                <h3 class="quote">Share Every Mile You've Go</h3>
+                <a href="#about" class="btn btn-dark btn-lg" id="start">Discover</a>
+            </div>
+        </header>
+        <hr>
+
+        <!-- Services Section -->
+        <section id="service">
+            <div class="section-content">
+                <h1 class="section-header">Login</h1>
+            </div>
+            <div class="modal-dialog">
+                <div class="loginmodal-container">
+                    <form id="login-form" method="POST" action="">
+                        <input type="email" name="user" placeholder="Email">
+                        <input type="password" name="pass" placeholder="Password">
+                        <input type="submit" name="login" class="login loginmodal-submit" value="Login">
+                    </form>
+
+                    <div class="login-help">
+                        <a href="register.php">Register</a> - <a href="#">Forgot Password</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- Portfolio Grid Section -->
+        <section id="portfolio" class="bg-light-gray">
+            <div class="section-content">
+                <h1 class="section-header">Register</h1>
+            </div>
+            <div class="contact-section">
+                <div class="container">
+                    <form id="register-form">
+                        <div class="container">
+                            <div class="form-group">
+                                <label for="exampleInputUsername" class="form-title">Your name</label>
+                                <input type="text" class="form-control" placeholder=" Enter Name">
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleInputEmail" class="form-title">Email Address</label>
+                                <input type="email" class="form-control" id="exampleInputEmail" placeholder=" Enter Email id">
+                            </div>	
+                            <div class="form-group">
+                                <label for="password" class="form-title">Password</label>
+                                <input type="password" class="form-control" id="password" placeholder="Not less than 6 characters, at least 1 integer and uppercase letter">
+                            </div>
+                            <div class="form-group">
+                                <label for="password" class="form-title">Confirm Password</label>
+                                <input type="password" class="form-control" id="confirm-password" placeholder="">
+                            </div>
+                            <div class="form-group">
+                                <label for ="gender" class="form-title">Gender</label>
+                                <select class="form-control">
+                                    <option>Male</option>
+                                    <option>Female</option>
+                                </select>
+                            </div>
+                            <div>
+                                <button type="button" class="button submit"><i class="fa fa-paper-plane" aria-hidden="true"></i>     Submit</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </section>
+
+        <!-- Team Section -->
+        <section id="team" class="bg-light-gray">
+            <div class="container">
+                <div class="row">
+                    <div class="section-content">
+                        <h1 class="section-header">Our Amazing Team</h1>
+                    </div>
+                </div>
+                <br><br><br>
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div class="team-member">
+                            <img src="img/team/1.jpg" class="img-responsive img-circle" alt="">
+                            <h4 style="color: white; text-shadow:5px 2px 10px #2c3e50">Jin</h4>
+                            <p class="text-muted" style="color: white; text-shadow:5px 2px 10px #2c3e50">Fron-end Designer</p>
+                            <ul class="list-inline social-buttons">
+                                <li><a href="#"><i class="fa fa-twitter"></i></a>
+                                </li>
+                                <li><a href="#"><i class="fa fa-facebook"></i></a>
+                                </li>
+                                <li><a href="#"><i class="fa fa-linkedin"></i></a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="team-member">
+                            <img src="img/team/2.jpg" class="img-responsive img-circle" alt="">
+                            <h4 style="color: white; text-shadow:5px 2px 10px #2c3e50">Kyle</h4>
+                            <p class="text-muted" style="color: white; text-shadow:5px 2px 10px #2c3e50">Back-end Coder</p>
+                            <ul class="list-inline social-buttons">
+                                <li><a href="#"><i class="fa fa-twitter"></i></a>
+                                </li>
+                                <li><a href="#"><i class="fa fa-facebook"></i></a>
+                                </li>
+                                <li><a href="#"><i class="fa fa-linkedin"></i></a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+        <!-- jQuery -->
+        <script src="vendor/jquery/jquery.min.js"></script>
+
+        <!-- Bootstrap Core JavaScript -->
+        <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+        <!-- Plugin JavaScript -->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js" integrity="sha384-mE6eXfrb8jxl0rzJDBRanYqgBxtJ6Unn4/1F7q4xRRyIw7Vdg9jP4ycT7x1iVsgb" crossorigin="anonymous"></script>
+
+        <!-- Contact Form JavaScript -->
+        <script src="js/jqBootstrapValidation.js"></script>
+        <script src="js/contact_me.js"></script>
+
+        <!-- Theme JavaScript -->
+        <script src="js/agency.min.js"></script>
+
+    </body>
+
+</html>
