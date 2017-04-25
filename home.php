1ccb1667398173ba100589bb2e1141a0616d93fb
Jinlim01 <jinlim0101@hotmail.com>
2017-04-25 13:37:43 +0100

    Add files via upload

---
+++ home.php
@@ -0,0 +1,48 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+
+        <title>Home</title>
+
+        <!-- Bootstrap Core CSS -->
+        <link href="css/bootstrap.min_1.css" rel="stylesheet">
+
+        <!-- Custom CSS -->
+        <link href="css/home.css" rel="stylesheet" type="text/css"/>
+
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+
+    </head>
+
+    <body> 
+        <?php include 'navbar.php'; ?>
+        <div class="container-fluid content" style="padding: 0">
+            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 gallery" style="padding: 0"><img class="img-responsive img" src="img/IMG_0408.JPG" alt=""/></div>
+            <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 gallery" style="padding: 0"><img class="img-responsive img" src="img/IMG_0408.JPG" alt=""/></div>
+        </div>
+
+        <!-- jQuery -->
+        <script src="js/jquery.js"></script>
+
+        <!-- Bootstrap Core JavaScript -->
+        <script src="js/bootstrap.min.js"></script>
+
+    </body>
+
+</html>
+
