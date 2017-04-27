f367712ca8d2c0316868e5e417224318154bc1f3
Jin <jinlim0101@hotmail.com>
2017-04-27 16:38:55 +0100

    allow comment btn

--- addNewPost.php
+++ addNewPost.php
@@ -20,53 +20,57 @@ and open the template in the editor.
         <link href="css/addNewPost.css" rel="stylesheet" type="text/css"/>
         <?php
         include 'navbar.php';
-        if ($L == TRUE) {
-            if (!empty($form_errors1)) {
-                print_r($form_errors1);
-            }
-            ?>
-            <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-            <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-            <!--[if lt IE 9]>
-                <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js" integrity="sha384-0s5Pv64cNZJieYFkXYOTId2HMA2Lfb6q2nAcx2n0RTLUnCAoTTsS0nKEO27XyKcY" crossorigin="anonymous"></script>
-                <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js" integrity="sha384-ZoaMbDF+4LeFxg6WdScQ9nnR1QC2MIRxA1O9KWEXQwns1G8UNyIEZIQidzb0T1fo" crossorigin="anonymous"></script>
-            <![endif]-->
+//        if ($L == TRUE) {
+//            if (!empty($form_errors1)) {
+//                print_r($form_errors1);
+        //}
+        ?>
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js" integrity="sha384-0s5Pv64cNZJieYFkXYOTId2HMA2Lfb6q2nAcx2n0RTLUnCAoTTsS0nKEO27XyKcY" crossorigin="anonymous"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js" integrity="sha384-ZoaMbDF+4LeFxg6WdScQ9nnR1QC2MIRxA1O9KWEXQwns1G8UNyIEZIQidzb0T1fo" crossorigin="anonymous"></script>
+        <![endif]-->
 
-        </head>
+    </head>
 
-        <body>
-            <div class="container">
-                <form id="newPost-form" method="post" action="processCreation.php" enctype="multipart/form-data">
-                    <div class="col-md-8 col-md-offset-2">
-                        <div class="form-group">
-                            <input type="file" class="form-control" name="picName" id="picName" >
-                        </div>
-                        <div class="form-group">
-                            <label for="exampleInputTitle" class="form-title">Title</label>
-                            <input type="text" class="form-control" id="post-title" name="title" placeholder=" Enter Title" value="">
-                        </div>	
-                        <div class="form-group">
-                            <label for="" class="form-title">Content</label>
-                            <textarea rows="20" type="text" class="form-control" id="about-you" name="content" placeholder=" Enter text here..." value=""></textarea>
-                        </div>
-                        <div class="form-group">
-                            <label for="" class="form-title">Hashtags</label>
-                            <input type="text" class="form-control" id="post-title" name="hashtags" placeholder=" Seperate your hashtags with comma" value="">
-                            <input type="hidden" name="userID" value="<?php echo $UID ?>">
-
-                        </div>
-                        <div>
-                            <center>
-                                <button type="submit" id="post_button" class="button submit"><i class="fa fa-plus-circle" aria-hidden="true"></i>&nbsp;&nbsp;Post</button>
-                            </center>
-                        </div>
+    <body>
+        <div class="container">
+            <form id="newPost-form" method="post" action="processCreation.php" enctype="multipart/form-data">
+                <div class="col-md-8 col-md-offset-2">
+                    <div class="form-group">
+                        <input type="file" class="form-control" name="picName" id="picName" >
                     </div>
-                </form>
-            </div>
-        </body>
-    </html>
-    <?php
-} else {
-    header('Location: home.php');
-}
+                    <div class="form-group">
+                        <label for="exampleInputTitle" class="form-title">Title</label>
+                        <input type="text" class="form-control" id="post-title" name="title" placeholder=" Enter Title" value="">
+                    </div>	
+                    <div class="form-group">
+                        <label for="" class="form-title">Content</label>
+                        <textarea rows="20" type="text" class="form-control" id="about-you" name="content" placeholder=" Enter text here..." value=""></textarea>
+                    </div>
+                    <div class="form-group">
+                        <label for="" class="form-title">Hashtags</label>
+                        <input type="text" class="form-control" id="post-title" name="hashtags" placeholder=" Seperate your hashtags with comma" value="">
+                        <input type="hidden" name="userID" value="<?php echo $UID ?>">
+                    </div>
+                    <div class="form-group">
+                        <label for="" class="form-title">Allow Comments</label><br>
+                        <input type="radio" name="comments" class="" value="Yes" checked>Yes<br>
+                        <input type="radio" name="comments" class="" value="No" checked>No
+                    </div>
+                    <div>
+                        <center>
+                            <button type="submit" id="post_button" class="button submit"><i class="fa fa-plus-circle" aria-hidden="true"></i>&nbsp;&nbsp;Post</button>
+                        </center>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </body>
+</html>
+<?php
+//} else {
+//    header('Location: home.php');
+//}
 ?>
---
+++ profile.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link href="css/profile.css" rel="stylesheet" type="text/css"/>
+        <title>Profile</title>
+    </head>
+    <body>
+        <?php include 'navbar.php'?>
+        
+        <div class="container-fluid big-container" style="padding: 0; top: 0; left: 0;">
+            <div class="col-md-5 col-sm-12 profile-container" style="padding: 0;">
+                <div class="col-md-6 col-md-offset-3" style="padding: 0; top: 10px;"><img class="profile-pic" src="img/team/1.jpg" alt=""/></div>
+            </div>
+        </div>
+    </body>
+</html>
