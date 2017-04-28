6107442214610712ab9b07a4eab421310afe1a87
Jin <jinlim0101@hotmail.com>
2017-04-28 18:17:20 +0100

    display posts page

--- post.php
+++ post.php
@@ -12,42 +12,71 @@ and open the template in the editor.
     </head>
     <body>
         <?php include 'navbar.php' ?>
+
         <div class="container-fluid" style="bottom: 0;">
-            <div class="row">
-                <div class="col-md-3 img-wrapper portfolio-item" style="padding: 0; margin-bottom: 0;">
-                    <a href="#">
-                        <img class="img-responsive posts" src="img/index3.JPG" alt="">
-                    </a>
-                    <h4 class="text">London Day Trip</h4>
-                    <span class="fa fa-thumbs-up fa-lg like">&nbsp;&nbsp; 86</span>
+            <div class="col-lg-12">
+                <h1 class="page-header">Thumbnail Gallery</h1>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-4 portfolio-item">
+                        <a href="#">
+                            <img class="img-responsive posts" src="img/index2.JPG" alt="">
+                        </a>
+                        <span class="fa fa-thumbs-up fa-lg like">&nbsp;&nbsp; 86</span>
+                        <h3>
+                            <a href="#" style="color: #000">Project Name</a>
+                        </h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
+                    </div>
+                    <div class="col-md-4 portfolio-item">
+                        <a href="#">
+                            <img class="img-responsive posts" src="img/index2.JPG" alt="">
+                        </a>
+                        <span class="fa fa-thumbs-up fa-lg like">&nbsp;&nbsp; 86</span>
+                        <h3>
+                            <a href="#" style="color: #000">Project Name</a>
+                        </h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
+                    </div>
+                    <div class="col-md-4 portfolio-item">
+                        <a href="#">
+                            <img class="img-responsive posts" src="img/index2.JPG" alt="">
+                        </a>
+                        <span class="fa fa-thumbs-up fa-lg like">&nbsp;&nbsp; 86</span>
+                        <h3>
+                            <a href="#" style="color: #000">Project Name</a>
+                        </h3>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
+                    </div>
                 </div>
-                <div class="col-md-3 img-wrapper portfolio-item" style="padding: 0; margin-bottom: 0;">
-                    <a href="#">
-                        <img class="img-responsive posts" src="img/index3.JPG" alt="">
-                    </a>
-                    <h4 class="text">London Day Trip</h4>
-                    <span class="fa fa-thumbs-up fa-lg like">&nbsp;&nbsp; 86</span>
-                </div>
-                <div class="col-md-3 img-wrapper portfolio-item" style="padding: 0; margin-bottom: 0;">
-                    <a href="#">
-                        <img class="img-responsive posts" src="img/index3.JPG" alt="">
-                    </a>
-                    <h4 class="text">London Day Trip</h4>
-                    <span class="fa fa-thumbs-up fa-lg like">&nbsp;&nbsp; 86</span>
-                </div>
-                <div class="col-md-3 img-wrapper portfolio-item" style="padding: 0; margin-bottom: 0;">
-                    <a href="#">
-                        <img class="img-responsive posts" src="img/index3.JPG" alt="">
-                    </a>
-                    <h4 class="text">London Day Trip</h4>
-                    <span class="fa fa-thumbs-up fa-lg like">&nbsp;&nbsp; 86</span>
-                </div>
-                <div class="col-md-3 img-wrapper portfolio-item" style="padding: 0; margin-bottom: 0;">
-                    <a href="#">
-                        <img class="img-responsive posts" src="img/index3.JPG" alt="">
-                    </a>
-                    <h4 class="text">London Day Trip</h4>
-                    <span class="fa fa-thumbs-up fa-lg like">&nbsp;&nbsp; 86</span>
+            </div>
+
+            <div class="row text-center">
+                <div class="col-lg-12">
+                    <ul class="pagination">
+                        <li>
+                            <a href="#">&laquo;</a>
+                        </li>
+                        <li class="active" style="color: #ffd11a !important;">
+                            <a href="#">1</a>
+                        </li>
+                        <li>
+                            <a href="#">2</a>
+                        </li>
+                        <li>
+                            <a href="#">3</a>
+                        </li>
+                        <li>
+                            <a href="#">4</a>
+                        </li>
+                        <li>
+                            <a href="#">5</a>
+                        </li>
+                        <li>
+                            <a href="#">&raquo;</a>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
