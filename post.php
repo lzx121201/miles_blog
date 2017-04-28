1fe2b9ca15db719ed7a6935b844ec973b6bd93cd
Jin <jinlim0101@hotmail.com>
2017-04-28 16:51:36 +0100

    display posts page

---
+++ post.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link href="css/post.css" rel="stylesheet" type="text/css"/>
+        <title></title>
+    </head>
+    <body>
+        <?php include 'navbar.php' ?>
+        <div class="container-fluid" style="bottom: 0;">
+            <div class="row">
+                <div class="col-md-3 img-wrapper portfolio-item" style="padding: 0; margin-bottom: 0;">
+                    <a href="#">
+                        <img class="img-responsive posts" src="img/index3.JPG" alt="">
+                    </a>
+                    <h4 class="text">London Day Trip</h4>
+                    <span class="fa fa-thumbs-up fa-lg like">&nbsp;&nbsp; 86</span>
+                </div>
+                <div class="col-md-3 img-wrapper portfolio-item" style="padding: 0; margin-bottom: 0;">
+                    <a href="#">
+                        <img class="img-responsive posts" src="img/index3.JPG" alt="">
+                    </a>
+                    <h4 class="text">London Day Trip</h4>
+                    <span class="fa fa-thumbs-up fa-lg like">&nbsp;&nbsp; 86</span>
+                </div>
+                <div class="col-md-3 img-wrapper portfolio-item" style="padding: 0; margin-bottom: 0;">
+                    <a href="#">
+                        <img class="img-responsive posts" src="img/index3.JPG" alt="">
+                    </a>
+                    <h4 class="text">London Day Trip</h4>
+                    <span class="fa fa-thumbs-up fa-lg like">&nbsp;&nbsp; 86</span>
+                </div>
+                <div class="col-md-3 img-wrapper portfolio-item" style="padding: 0; margin-bottom: 0;">
+                    <a href="#">
+                        <img class="img-responsive posts" src="img/index3.JPG" alt="">
+                    </a>
+                    <h4 class="text">London Day Trip</h4>
+                    <span class="fa fa-thumbs-up fa-lg like">&nbsp;&nbsp; 86</span>
+                </div>
+                <div class="col-md-3 img-wrapper portfolio-item" style="padding: 0; margin-bottom: 0;">
+                    <a href="#">
+                        <img class="img-responsive posts" src="img/index3.JPG" alt="">
+                    </a>
+                    <h4 class="text">London Day Trip</h4>
+                    <span class="fa fa-thumbs-up fa-lg like">&nbsp;&nbsp; 86</span>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
