befacc11a6b41507348e28a0fb3c1f80aa21a50c
Jin <jinlim0101@hotmail.com>
2017-04-29 12:36:21 +0100

    search box

--- allPost.php
+++ allPost.php
@@ -23,26 +23,48 @@ and open the template in the editor.
             <div class="col-lg-12">
                 <h1 class="page-header">All Posts</h1>
             </div>
+            <div class="col-md-12 col-md-offset-1">
+                <form>
+                    <div class="col-md-2 form-group searchbox">
+                        <input type="text" class="form-control" name="" id="exampleInputUsername" placeholder=" Search username" value="">
+                    </div>
+                    <div class="col-md-2 form-group searchbox">
+                        <input type="text" class="form-control" name="" id="exampleInputUsername" placeholder=" Search Tags" value="">
+                    </div>
+                    <div class="col-md-2 form-group searchbox">
+                        <input type="text" class="form-control" name="" id="exampleInputUsername" placeholder=" Search Keywords" value="">
+                    </div>
+                    <div class="col-md-2 form-group searchbox">
+                        <input type="date" class="form-control" name="" id="exampleInputUsername" placeholder=" From" value="">
+                    </div>
+                    <div class="col-md-2 form-group searchbox">
+                        <input type="date" class="form-control" name="" id="exampleInputUsername" placeholder=" To" value="">
+                    </div>
+                    <div class="col-md-2 form-group searchbox">
+                        <button class="search-btn"><i class="fa fa-search search-icon"></i></button>
+                    </div>
+                </form>
+            </div>
+
             <div class="container">
                 <?php
-                
                 if (isset($_GET['page'])) {
                     $page = filter_input(INPUT_GET, "page", FILTER_SANITIZE_NUMBER_INT);
+                } else {
+                    $page = 1;
                 }
-                else
-                    {
-                    $page=1;
-                    }
                 $query = "SELECT * FROM post ORDER BY PostID";
-        $statement = $db->prepare($query);
-        $statement->execute();
-        $result = $statement->fetchALL(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, 'Post');
-        $statement->closeCursor();
+                $statement = $db->prepare($query);
+                $statement->execute();
+                $result = $statement->fetchALL(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, 'Post');
+                $statement->closeCursor();
                 $posts = array(array());
 
-                $i = 0;$j = 0;$count = 0;
+                $i = 0;
+                $j = 0;
+                $count = 0;
                 while ($count < sizeof($result)) {
-                    if ($count % (MAX_ROW_AT_ALL*MAX_POST_PER_ROW )== 0 && $count != 0) {
+                    if ($count % (MAX_ROW_AT_ALL * MAX_POST_PER_ROW ) == 0 && $count != 0) {
                         $i++;
                         $j = 0;
                     }
@@ -52,90 +74,80 @@ and open the template in the editor.
                 }
                 $pages = sizeof($posts);
                 $a = 0;
-                $per_page = array_chunk($posts[$page-1],MAX_POST_PER_ROW);
-                while($a< sizeof($per_page))
-                {
+                $per_page = array_chunk($posts[$page - 1], MAX_POST_PER_ROW);
+                while ($a < sizeof($per_page)) {
                     echo '<div class="row">';
-                    foreach ($per_page[$a] as $p)
-                    {
+                    foreach ($per_page[$a] as $p) {
                         echo $p->displayAtAllPost();
                     }
                     echo '</div>';
                     $a++;
                 }
                 ?>
-<!--                <div class="row">
-                    <div class="col-md-4 portfolio-item">
-                        <a href="#">
-                            <img class="img-responsive posts" src="img/index2.JPG" alt="">
-                        </a>
-                        <span class="fa fa-thumbs-up fa-lg like">&nbsp;&nbsp; 86</span>
-                        <h3>
-                            <a href="#" style="color: #000">Project Name</a>
-                        </h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
-                    </div>
-                    <div class="col-md-4 portfolio-item">
-                        <a href="#">
-                            <img class="img-responsive posts" src="img/index2.JPG" alt="">
-                        </a>
-                        <span class="fa fa-thumbs-up fa-lg like">&nbsp;&nbsp; 86</span>
-                        <h3>
-                            <a href="#" style="color: #000">Project Name</a>
-                        </h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
-                    </div>
-                    <div class="col-md-4 portfolio-item">
-                        <a href="#">
-                            <img class="img-responsive posts" src="img/index2.JPG" alt="">
-                        </a>
-                        <span class="fa fa-thumbs-up fa-lg like">&nbsp;&nbsp; 86</span>
-                        <h3>
-                            <a href="#" style="color: #000">Project Name</a>
-                        </h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
-                    </div>
-                </div>-->
+                <!--                <div class="row">
+                                    <div class="col-md-4 portfolio-item">
+                                        <a href="#">
+                                            <img class="img-responsive posts" src="img/index2.JPG" alt="">
+                                        </a>
+                                        <span class="fa fa-thumbs-up fa-lg like">&nbsp;&nbsp; 86</span>
+                                        <h3>
+                                            <a href="#" style="color: #000">Project Name</a>
+                                        </h3>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
+                                    </div>
+                                    <div class="col-md-4 portfolio-item">
+                                        <a href="#">
+                                            <img class="img-responsive posts" src="img/index2.JPG" alt="">
+                                        </a>
+                                        <span class="fa fa-thumbs-up fa-lg like">&nbsp;&nbsp; 86</span>
+                                        <h3>
+                                            <a href="#" style="color: #000">Project Name</a>
+                                        </h3>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
+                                    </div>
+                                    <div class="col-md-4 portfolio-item">
+                                        <a href="#">
+                                            <img class="img-responsive posts" src="img/index2.JPG" alt="">
+                                        </a>
+                                        <span class="fa fa-thumbs-up fa-lg like">&nbsp;&nbsp; 86</span>
+                                        <h3>
+                                            <a href="#" style="color: #000">Project Name</a>
+                                        </h3>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
+                                    </div>
+                                </div>-->
             </div>
 
             <div class="row text-center">
                 <div class="col-lg-12">
                     <ul class="pagination">
-                        <?php 
-                        $num_of_page = sizeof($posts);
-                        $previous_page = $page-1;
-                        if($previous_page<1)
-                        {
-                            $previous_page = 1;
-                        }
-                        $next_page = $page+1;
-                        if($next_page>$num_of_page)
-                        {
-                            $next_page = $num_of_page;
-                        }
-                            
-                        ?>
+<?php
+$num_of_page = sizeof($posts);
+$previous_page = $page - 1;
+if ($previous_page < 1) {
+    $previous_page = 1;
+}
+$next_page = $page + 1;
+if ($next_page > $num_of_page) {
+    $next_page = $num_of_page;
+}
+?>
                         <li>
-                            <a href="allPost.php?page=<?php echo $previous_page ;?>">&laquo;</a>
+                            <a href="allPost.php?page=<?php echo $previous_page; ?>">&laquo;</a>
                         </li>
                         <?php
-                        $b=0;
-                            while($b<$num_of_page)
-                            {
-                                if($b+1==$page)
-                                {
-                                    echo '<li class="active" style="color: #ffd11a !important;"><a href="allPost.php?page='.($b+1).'">'.($b+1).'</a></li>';
-                                }
-                                else
-                                {
-                                    echo '<li><a href="allPost.php?page='.($b+1).'">'.($b+1).'</a></li>';
-                                }
-                                $b++;
-                                
+                        $b = 0;
+                        while ($b < $num_of_page) {
+                            if ($b + 1 == $page) {
+                                echo '<li class="active" style="color: #ffd11a !important;"><a href="allPost.php?page=' . ($b + 1) . '">' . ($b + 1) . '</a></li>';
+                            } else {
+                                echo '<li><a href="allPost.php?page=' . ($b + 1) . '">' . ($b + 1) . '</a></li>';
                             }
+                            $b++;
+                        }
                         ?>           
                         <li>
-                            <a href="allPost.php?page=<?php echo $next_page ;?>">&raquo;</a>
+                            <a href="allPost.php?page=<?php echo $next_page; ?>">&raquo;</a>
                         </li>
                     </ul>
                 </div>
